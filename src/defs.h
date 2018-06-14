c4799f5e7e4d48bdf30bba65a7786e33b79fd8f0
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2018-06-14 12:18:19 +0200

    Add functions to generate and print configurations

--- src/defs.h
+++ src/defs.h
@@ -10,15 +10,27 @@
 
 #define SQR(x) ((x) * (x))
 #define CUB(x) ((x) * (x) * (x))
+#define DOT(v, w) ((v).x * (w).x + (v).y * (w).y + (v).z * (w).z)
 
 #define NO_OVERLAP 0
 #define OVERLAP 1
 
 #define RCUT 2.5
-#define OVERLAP_THRESHOLD 0.9
+#define OVERLAP_THRESHOLD 0.95
 #define TEMPERATURE 1.0
 
 typedef double number;
-typedef number vector[3];
+
+typedef struct {
+	number x, y, z;
+} vector;
+
+typedef struct {
+	int N;
+	number box_side;
+	vector *positions;
+	vector *velocities;
+	vector *forces;
+} MD_system;
 
 #endif /* SRC_DEFS_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -28,11 +28,59 @@ int LJ_interaction(number r_sqr, number *energy, number *force_module_over_r) {
 	return NO_OVERLAP;
 }
 
-void init_configuration(int N, number box_side, vector *positions) {
+void init_configuration(MD_system *syst) {
 	int i = 0;
-	for(i = 0; i < N; i++) {
+	for(i = 0; i < syst->N; i++) {
+		int done = 0;
+		vector trial_position;
+		while(!done) {
+			trial_position.x = drand48() * syst->box_side;
+			trial_position.y = drand48() * syst->box_side;
+			trial_position.z = drand48() * syst->box_side;
+			// N^2 check to look for "overlaps"
+			int j = 0;
+			done = 1;
+			for(j = 0; j < i && done; j++) {
+				vector *other_position = syst->positions + j;
+				vector dist = {
+						trial_position.x - other_position->x,
+						trial_position.y - other_position->y,
+						trial_position.z - other_position->z
+				};
+				dist.x -= syst->box_side * rint(dist.x / syst->box_side);
+				dist.y -= syst->box_side * rint(dist.y / syst->box_side);
+				dist.z -= syst->box_side * rint(dist.z / syst->box_side);
 
+				number r_sqr = DOT(dist, dist);
+				number energy, force;
+				if(LJ_interaction(r_sqr, &energy, &force) == OVERLAP) {
+					done = 0;
+				}
+			}
+		}
+
+		syst->positions[i] = trial_position;
+
+		if(syst->N > 10 && i % (syst->N/10) == 0) {
+			fprintf(stderr, "Inserted %d%% of the particles (%d/%d)\n", i*100/syst->N, i, syst->N);
+		}
 	}
+}
+
+void print_cogli1_configuration(MD_system *syst, char *filename) {
+	FILE *out = fopen(filename, "w");
+	if(out == NULL) {
+		fprintf(stderr, "File '%s' is not writable", filename);
+		exit(1);
+	}
+
+	fprintf(out, ".Box:%lf,%lf,%lf\n", syst->box_side, syst->box_side, syst->box_side);
+	int i;
+	for(i = 0; i < syst->N; i++) {
+		fprintf(out, "%lf %lf %lf @ 0.5 C[blue]\n", syst->positions[i].x, syst->positions[i].y, syst->positions[i].z);
+	}
+
+	fclose(out);
 }
 
 int main(int argc, char *argv[]) {
@@ -43,19 +91,23 @@ int main(int argc, char *argv[]) {
 
 	srand48(13245124);
 
-	int N = atoi(argv[1]);
+	MD_system syst;
+
+	syst.N = atoi(argv[1]);
 	number density = atof(argv[2]);
-	number box_side = pow(N / density, 1./3.);
+	syst.box_side = pow(syst.N / density, 1./3.);
 
-	vector *positions = (vector *) malloc(sizeof(vector) * N);
-	vector *velocities = (vector *) malloc(sizeof(vector) * N);
-	vector *forces = (vector *) malloc(sizeof(vector) * N);
+	syst.positions = (vector *) malloc(sizeof(vector) * syst.N);
+	syst.velocities = (vector *) malloc(sizeof(vector) * syst.N);
+	syst.forces = (vector *) malloc(sizeof(vector) * syst.N);
 
-	init_configuration(N, box_side, positions);
+	init_configuration(&syst);
+	// the explicit cast is there to remove a warning issued by g++
+	print_cogli1_configuration(&syst, (char *)"initial.mgl");
 
-	free(forces);
-	free(velocities);
-	free(positions);
+	free(syst.forces);
+	free(syst.velocities);
+	free(syst.positions);
 
 	return 0;
 }
